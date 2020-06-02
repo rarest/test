15299423674a1596efa5a06edd43707acf4e9df5
chenshuquan <chenshuquan@kuaishou.com>
2020-06-02 14:03:32 +0800

    thread pool

--- cpp/threadPool/threadPool.cpp
+++ cpp/threadPool/threadPool.cpp
@@ -3,3 +3,85 @@
 //
 
 #include "threadPool.h"
+
+
+#include <thread>
+#include <iostream>
+#include <mutex>
+
+static std::once_flag g_once_flag;
+ThreadPool *ThreadPool::instance = nullptr;
+std::mutex ThreadPool::instanceMutex;
+
+void ThreadPool::start() {
+    if (alive || runningJobs > 0 || aliveThreads > 0) return;
+    pool.clear();
+    alive = true;
+    aliveThreads = 0;
+    printf("threading pool size %u\n", threadNum);
+    for (decltype(threadNum) ii = 0; ii < threadNum; ii++) {
+        pool.emplace_back(std::thread(&ThreadPool::workingFunction, this));
+        aliveThreads += 1;
+    }
+
+}
+
+void ThreadPool::addJob(std::function<void()> job) {
+    if (!alive) return;
+    {
+        std::unique_lock<std::mutex> lock(quequeMutex);
+        queue.emplace(job);
+    }
+    queueCondition.notify_one();
+
+}
+
+void ThreadPool::waitFinish() {
+    std::unique_lock<std::mutex> lock(taskMutex);
+    taskCondition.wait(lock, [this] { return !taskRunning(); });
+}
+
+bool ThreadPool::taskRunning() {
+    return runningJobs > 0 || !queue.empty();
+}
+
+ThreadPool *ThreadPool::getInstance() {
+    std::call_once(g_once_flag, [&]() { instance = new ThreadPool();});
+    return instance;
+}
+
+void ThreadPool::workingFunction() {
+    std::unique_lock<std::mutex> lock(quequeMutex);
+    while (true) {
+        if (!queue.empty()) {
+            std::function<void()> job = queue.front();
+            queue.pop();
+            lock.unlock();
+            {
+                std::unique_lock<std::mutex> lk(taskMutex);
+                runningJobs += 1;
+            }
+            job();
+            {
+                std::unique_lock<std::mutex> lk(taskMutex);
+                runningJobs -= 1;
+            }
+
+        } else if (!alive) {
+            aliveThreads -= 1;
+            taskCondition.notify_one();
+            queueCondition.notify_one();
+            break;
+        } else {
+            taskCondition.notify_one();
+            queueCondition.wait(lock, [this] { return !this->queue.empty() || !this->alive; });
+        }
+    }
+}
+
+
+ThreadPool::ThreadPool() : threadNum(std::thread::hardware_concurrency()), alive(false), runningJobs(0),
+                           aliveThreads(0) {
+    start();
+}
+
--- cpp/threadPool/threadPool.h
+++ cpp/threadPool/threadPool.h
@@ -5,9 +5,41 @@
 #ifndef INTERVIEW_THREADPOOL_H
 #define INTERVIEW_THREADPOOL_H
 
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <functional>
+#include <future>
+#include <vector>
+#include <queue>
 
 class ThreadPool {
+public:
+    ~ThreadPool() = default;
+    void start();
+    void addJob(std::function<void()> job);
+    void waitFinish();
+    bool taskRunning();
+    static ThreadPool *getInstance();
 
+private:
+    ThreadPool();
+    unsigned int threadNum;
+    std::vector<std::thread > pool;
+    std::atomic<bool> alive;
+    std::queue<std::function<void() > >queue;
+    std::mutex quequeMutex;
+    std::condition_variable queueCondition;
+
+    std::atomic<int> runningJobs;
+    std::atomic<int> aliveThreads;
+
+    std::mutex taskMutex;
+    std::condition_variable taskCondition;
+
+    void workingFunction();
+    static ThreadPool *instance;
+    static std::mutex instanceMutex;
 };
 
 
